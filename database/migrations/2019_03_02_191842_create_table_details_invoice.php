ed8dbb3e5d995824b28fd1fe5354f37c9c9ca2a9
FuriosoJack <iam@furiosojack.com>
2019-03-02 14:53:42 -0500

    Se añaden migraciones

---
+++ database/migrations/2019_03_02_191842_create_table_details_invoice.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableDetailsInvoice extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('details_invoice', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("id_invoice")->unsigned();
+            $table->foreign("id_invoice")->references("id")->on("invoices");
+            $table->integer("id_inventory")->unsigned();
+            $table->foreign("id_inventory")->references("id")->on("inventory");
+            $table->integer("quantiry");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('details_invoice');
+    }
+}
